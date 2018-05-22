6dfa89d052b59bc05935d2b84e4ae70a22fbbe2f
amoinier <amoinier@outlook.fr>
2018-05-23 00:03:03 +0200

    update

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
 sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc 
-docker docker-compose docker-machine libsynaptics xf86-input-synaptics
+docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox 
+linux-headers
 yaourt -S spotify visual-studio-code-bin 

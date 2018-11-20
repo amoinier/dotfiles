71af7a522956bb0da58e8e31340daa8b918866d7
amoinier <amoinier@outlook.fr>
2018-11-20 12:38:42 +0100

    add copy isntaller

--- installer.sh
+++ installer.sh
@@ -1,2 +1,3 @@
 sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox linux-headers yaourt rofi nodejs npm blueman deluge nginx mongodb
 yaourt -S spotify visual-studio-code-bin 
+cp -R ./.config ~/

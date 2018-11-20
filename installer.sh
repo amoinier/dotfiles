be6609d3743b30ef48da90a1e53c71b8edb60d45
amoinier <amoinier@outlook.fr>
2018-11-20 12:33:31 +0100

    init v2

--- installer.sh
+++ installer.sh
@@ -1,4 +1,2 @@
-sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc 
-docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox 
-linux-headers
+sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox linux-headers
 yaourt -S spotify visual-studio-code-bin 

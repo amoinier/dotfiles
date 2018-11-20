9b01698087b085b8086fe4fc1a01db3e83dd99d4
amoinier <amoinier@outlook.fr>
2018-11-20 12:36:13 +0100

    add installer

--- installer.sh
+++ installer.sh
@@ -1,2 +1,2 @@
-sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox linux-headers
+sudo pacman -S i3-gaps i3lock i3blocks conky nginx mongodb playerctl dmenu acpi terminator vlc docker docker-compose docker-machine libsynaptics xf86-input-synaptics rhythmbox virtualbox linux-headers yaourt rofi nodejs npm blueman deluge nginx mongodb
 yaourt -S spotify visual-studio-code-bin 
